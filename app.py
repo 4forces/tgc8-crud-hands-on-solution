1b94155fb0bc75ea6e5f669c34a7d959d99036c9
kunxin-chor <chorkunxin@yahoo.com>
2020-09-26 08:28:26 +0000

    Added edit

--- app.py
+++ app.py
@@ -37,6 +37,48 @@ def process_add_food():
     return redirect(url_for('show_food'))
 
 
+@app.route('/foods/<int:food_id>/edit')
+def show_edit_food(food_id):
+
+    # 1. find the food record to edit
+    food_to_edit = None
+    for each_food in database:
+        if each_food["id"] == food_id:
+            food_to_edit = each_food
+            break
+
+    if food_to_edit:
+        return render_template('edit_food.template.html', food=food_to_edit)
+    else:
+        return f"The food record with the id of {food_id} is not found!"
+
+
+@app.route('/foods/<int:food_id>/edit', methods=["POST"])
+def process_edit_food(food_id):
+    # 1. find the food record to edit
+    food_to_edit = None
+    for each_food in database:
+        if each_food["id"] == food_id:
+            food_to_edit = each_food
+            break
+
+    if food_to_edit:
+        food_to_edit['name'] = request.form.get('food_name')
+        food_to_edit["calories"] = request.form.get('calories')
+        food_to_edit['date'] = request.form.get('date')
+        food_to_edit['meal'] = request.form.get('meal')
+
+        with open('food.json', 'w') as fp:
+            json.dump(database, fp)
+
+        return redirect(url_for('show_food'))
+
+    else:
+         return f"The food record with the id of {food_id} is not found!"
+
+
+
+
 # "magic code" -- boilerplate
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
